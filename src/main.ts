cc65cccd1aad914b4a61f2d339f6b8ff33939958
exigoll <exigoll@bk.ru>
2023-04-14 16:58:49 +0500

    test

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { AppModule } from '@/modules/app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: ['http://localhost:5173'],
+      origin: ['http://45.141.76.28:4173'],
     },
   });
   const configService = app.get(ConfigService);
